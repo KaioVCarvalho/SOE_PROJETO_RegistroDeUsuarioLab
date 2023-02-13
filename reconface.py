5c2d08d87c66544c214505dfda16a19c06415b64
KaioVCarvalho <103061389+KaioVCarvalho@users.noreply.github.com>
2023-02-12 22:06:01 -0300

    Update reconface.py

--- reconface.py
+++ reconface.py
@@ -4,23 +4,28 @@ import face_recognition
 
 video_capture = cv2.VideoCapture(0)
 
-# Carregando e reconhecendo a face do usuário 1.
-user1_image = face_recognition.load_image_file("./img/180065416.jpg")
-user1_face_encoding = face_recognition.face_encodings(user1_image)[0]
+def generate_face_encodings(file):
+    with open(file, "r") as f:
+        lines = f.readlines()
 
-# Carregando e reconhecendo a face do usuário 2.
-user2_image = face_recognition.load_image_file("./img/180034090.jpg")
-user2_face_encoding = face_recognition.face_encodings(user2_image)[0]
+    known_face_encodings = []
+    for line in lines:
+        matricula = line.strip()
+        image_path = f"./img/{matricula}.jpg"
+        image = face_recognition.load_image_file(image_path)
+        encoding = face_recognition.face_encodings(image)[0]
+        known_face_encodings.append(encoding)
 
-# Criando array de associação das facer reconhecidas.
-known_face_encodings = [
-    user1_face_encoding,
-    user2_face_encoding
-]
-known_face_names = [
-    "180065416",
-    "180034090"
-]
+    return known_face_encodings
+
+def read_file(file):
+    with open(file, "r") as f:
+        lines = f.readlines()
+    return [line.strip() for line in lines]
+
+known_face_names = read_file("mat.txt")
+
+known_face_encodings = generate_face_encodings("mat.txt")
 
 # Initialize some variables
 face_locations = []
@@ -61,29 +66,4 @@ while True:
     process_this_frame = not process_this_frame
 
 
-#    # Display the results
-#    for (top, right, bottom, left), name in zip(face_locations, face_names):
-#        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-#        top *= 4
-#        right *= 4
-#        bottom *= 4
-#        left *= 4
-#
-#        # Draw a box around the face
-#        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-#
-#        # Draw a label with a name below the face
-#        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-#        font = cv2.FONT_HERSHEY_DUPLEX
-#        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-#
-#    # Display the resulting image
-#    cv2.imshow('Video', frame)
-#
-#    # Hit 'q' on the keyboard to quit!
-#    if cv2.waitKey(1) & 0xFF == ord('q'):
-#        break
-#
-## Release handle to the webcam
-#video_capture.release()
-#cv2.destroyAllWindows()
+
